414998d7f0efaaebd69f99bbc4a0f5eae62cfaff
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2023-12-31 14:44:57 +0100

    Инициализация, создание HomeController, страницы home.html, теста HomeControllerTest

---
+++ src/main/java/pizzamarket/HomeController.java
@@ -0,0 +1,13 @@
+package pizzamarket;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //контроллер
+public class HomeController {
+
+    @GetMapping("/") //обрабатывает запросы с корневым путем
+    public String home(){
+        return "home"; //возвращает имя представления
+    }
+}
