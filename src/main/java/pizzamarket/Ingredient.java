115be5c14f7d3e5f24f66aacfc165a32c8a65daa
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2023-12-31 17:41:12 +0100

    Создан класс Ingredient (ингредиенты)

---
+++ src/main/java/pizzamarket/Ingredient.java
@@ -0,0 +1,15 @@
+package pizzamarket;
+
+import lombok.Data;
+
+import java.lang.reflect.Type;
+@Data
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public enum Type{
+        BASIS, CHEESE, SAUCE, BACON, MUSHROOMS
+    }
+}
