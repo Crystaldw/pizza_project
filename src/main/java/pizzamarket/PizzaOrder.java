31b527726a6bc79cc6096519b4888365f87e4398
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2024-01-03 17:49:15 +0100

    Создана страница создания заказа пицы orderForm.html и PostMapping метод в OrderController

--- src/main/java/pizzamarket/PizzaOrder.java
+++ src/main/java/pizzamarket/PizzaOrder.java
@@ -1,11 +1,14 @@
 package pizzamarket;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
-
+@Data
 public class PizzaOrder {
     private String deliveryName;
     private String deliveryStreet;
+    private String deliveryCity;
     private String deliveryState;
     private String deliveryZip;
     private String ccNumber;
