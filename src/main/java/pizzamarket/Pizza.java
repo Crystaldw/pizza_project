96b29772e5b70d066e89d2c1171b63ee2cee4274
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2024-01-03 22:13:15 +0100

    Spring Validation dependency добавлена валидация в Pizza, PizzaOrder

--- src/main/java/pizzamarket/Pizza.java
+++ src/main/java/pizzamarket/Pizza.java
@@ -1,10 +1,16 @@
 package pizzamarket;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
 @Data
 public class Pizza {
+    @NotNull
+    @Size(min = 5, message ="You must choose at least 1 ingredient")
     private String name;
+    @NotNull
+    @Size(min=1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 }
