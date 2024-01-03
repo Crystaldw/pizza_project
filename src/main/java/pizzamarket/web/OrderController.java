1d1fdf7fc636787f844d112e6f4908b8156cc138
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2024-01-03 21:48:49 +0100

    Spring Validation dependency

--- src/main/java/pizzamarket/web/OrderController.java
+++ src/main/java/pizzamarket/web/OrderController.java
@@ -1,6 +1,6 @@
 package pizzamarket.web;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,20 +9,21 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import pizzamarket.PizzaOrder;
 
-@Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("pizzaOrder")
 public class OrderController {
 
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(OrderController.class);
+
     @GetMapping("/current")
-    public String orderForm(){
+    public String orderForm() {
         return "orderForm";
     }
 
     @PostMapping
     public String processOrder(PizzaOrder order,
-                               SessionStatus sessionStatus){
+                               SessionStatus sessionStatus) {
         log.info("Order Submitted: {}", order);
         sessionStatus.setComplete();
 
--- src/test/java/pizzamarket/HomeControllerTest.java
+++ src/test/java/pizzamarket/HomeControllerTest.java
@@ -1,25 +1,29 @@
 package pizzamarket;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import pizzamarket.web.HomeController;
 
 @WebMvcTest(HomeController.class) //тест для HomeController
 public class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc; //Внедрить MockMvc
+
     @Test
     public void testHomePage() throws Exception {
         mockMvc.perform(get("/"))   //Выполнить запрос GET /
                 .andExpect(status().isOk())  //Ожидается код ответа HTTP 200
                 .andExpect(view().name("home")) //Ожидается имя представления home
-                .andExpect((ResultMatcher) content().string(containsString("Welcome to...")));//Ожидается наличие строки «Welcome to...»
-    } }
+                .andExpect((ResultMatcher) content()
+                        .string(containsString("Welcome to...")));//Ожидается наличие строки «Welcome to...»
+    }
+}
