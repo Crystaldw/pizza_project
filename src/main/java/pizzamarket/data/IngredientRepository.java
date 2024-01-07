a1d948b856c16755a7f8e001575eee091322b213
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2024-01-07 15:46:25 +0100

    Создан Repository - JdbcIngredientRepository

---
+++ src/main/java/pizzamarket/data/IngredientRepository.java
@@ -0,0 +1,11 @@
+package pizzamarket.data;
+
+import pizzamarket.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+    Iterable<Ingredient> findAll();
+    Optional<Ingredient> findById(String id);
+    Ingredient save (Ingredient ingredient);
+}
